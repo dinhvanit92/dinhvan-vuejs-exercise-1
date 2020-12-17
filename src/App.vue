0cccf3c757000deef9cc087f74da89950b1144f3
Van Tran <dinhvan.it92@gmail.com>
2020-12-17 14:32:56 +0700

    Format default code

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div id="app">
+  <todos />
+</div>
 </template>
+<script>
+import Todos from './views/Todos.vue'
+export default {
+  name: 'App',
+  components: { Todos }
+  
+  
+}
+</script>
 
 <style>
+*{margin: 0;padding: 0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/Todos.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>todo list</p>
+</template>
+
+<script>
+export default {
+    name: 'Todos'
+
+}
+</script>
+
+<style>
+
+</style>
