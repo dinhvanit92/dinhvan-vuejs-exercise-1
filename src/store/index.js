5db888ec584c7fe79c83b43d1d04060af420d59a
Van Tran <dinhvan.it92@gmail.com>
2020-12-18 11:37:55 +0700

    commit deleted demo todo data

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,7 @@ import datetime from '../libs/setdatetime'
 
 const storeData = {
   state: {
-    todos: [
-      // {id: 1, create_time: '13:10 PM - 15/08/2020', task: 'công việc 1', completed: true, edit: false},
-      // {id: 2, create_time: '13:11 PM - 15/08/2020', task: 'công việc 2', completed: true, edit: false}
-    ]
+    todos: []
   },
   actions: {
     async gettodoRequest({ commit }) {
